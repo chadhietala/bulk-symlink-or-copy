81d4cea19a18839b0549779abfc5ab6b0dbeda17
Chad Hietala <chadhietala@gmail.com>
2015-09-08 21:09:49 -0700

    WIP

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ module.exports = function(paths) {
   var count = 0;
   var cache = {};
   var map = {};
+  var linked = {};
 
   paths.map(function(paths) {
     return Object.keys(paths)[0];
-  }).forEach(function(srcPath) {
+  }).forEach(function(srcPath, i) {
 
     var lstat = fs.lstatSync(srcPath);
 
@@ -49,67 +50,9 @@ module.exports = function(paths) {
       srcPath = process.cwd() + '/' + srcPath;
     }
 
-    if (cache[srcPath]) {
-      fs.symlinkSync(srcPath, paths[srcPath]);
+    if (!linked[srcPath]) {
+      fs.symlinkSync(srcPath, paths[i][srcPath]);
+      linked[srcPath] = true;
     }
   });
-
-  // paths.forEach(function(paths) {
-  //   var src = Object.keys(paths)[0];
-  //   var srcDir = path.dirname(src);
-  //   fs.symlinkSync(src, paths[src]);
-  // });
-
-
-
-  // paths.forEach(function(paths) {
-  //   var src = Object.keys(paths)[0];
-  //   var srcDir = path.dirname(src);
-  //   var dest = paths[src];
-  //
-  //   srcDir.split('/').reduce(function(current, next) {
-  //     if (current) {
-  //       current = current + '/' + next;
-  //     } else {
-  //       current = '/' + next;
-  //     }
-  //
-  //     if (!current)        { return; }
-  //     if (current === '.') { return; }
-  //     if (current === '')  { return; }
-  //     if (current === '/') { return; }
-  //
-  //     if (!created[current]) {
-  //       try {
-  //         count++;
-  //         fs.realpathSync(current, created);
-  //       } catch(e) {
-  //         if (e.code !== 'EEXIST') {
-  //           throw e;
-  //         }
-  //       }
-  //     }
-  //
-  //     return current;
-  //
-  //   }, '');
-  // });
-
-  // paths.forEach(function(paths) {
-  //   var src = Object.keys(paths)[0];
-  //   var srcDir = path.dirname(src);
-  //   fs.symlinkSync(src, paths[src]);
-  // });
-
-  // console.log(count, paths.length, f.length, unique(paths.map(function(item) {
-  //   return Object.keys(item)[0];
-  // })).length);
-  // for each tople
-
-  // for (var i = 0, l = paths.length; i < l; i++) {
-  //   var path = paths[i];
-  //   var srcPath = fs.realpathSync(path, cache);
-  //   cache[path] = srcPath;
-  //   fs.symlinkSync(srcPath, destinations[i]);
-  // }
 };
